e88beb8109b08cdc15a15b30f644ea8da03b4640
Sowparna Shome <soupornoshome@gmail.com>
2025-02-20 21:00:24 +0530

    login check

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
 
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const navigate = useRouter();
   return (
-    <>
-      <h1>This is just home okay</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-400 p-4">
+      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm text-center">
+        <h1 className="text-2xl font-semibold text-gray-700 mb-4">Welcome to Next.js</h1>
+        <button
+          onClick={() => navigate.push("/login")}
+          className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600"
+        >
+          Login
+        </button>
+      </div>
+    </div>
   );
 }
