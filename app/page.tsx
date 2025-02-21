05513e3018fd8f0564b709bcaf6b3c4267d9e0a4
Sowparna Shome <soupornoshome@gmail.com>
2025-02-21 23:16:06 +0530

    register button add

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,21 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-400 p-4">
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm text-center">
-        <h1 className="text-2xl font-semibold text-gray-700 mb-4">Welcome to Next.js</h1>
-        <button
-          onClick={() => navigate.push("/login")}
-          className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600"
-        >
-          Login
-        </button>
+        <h1 className="text-2xl font-semibold text-gray-700 mb-4">Welcome to Imacon</h1>
+        <div className="flex justify-between gap-x-4">
+          <button
+            onClick={() => navigate.push("/login")}
+            className="bg-blue-500 text-white py-2 px-6 rounded-lg hover:bg-blue-600 flex-1"
+          >
+            Login
+          </button>
+          <button
+            onClick={() => navigate.push("/register")}
+            className="bg-red-500 text-white py-2 px-6 rounded-lg hover:bg-red-600 flex-1"
+          >
+            Register
+          </button>
+        </div>
       </div>
     </div>
   );
