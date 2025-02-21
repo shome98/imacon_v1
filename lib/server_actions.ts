d319cf204a0a54a9bf9f00e151f798e1c20c0cdf
Sowparna Shome <soupornoshome@gmail.com>
2025-02-21 23:09:40 +0530

    comment out

--- lib/server_actions.ts
+++ lib/server_actions.ts
@@ -1,42 +1,42 @@
-"use server";
-import { IUser } from "@/models/User.model";
+// "use server";
+// import { IUser } from "@/models/User.model";
 
-type FetchOptions = {
-  method?: "GET" | "POST" | "PUT" | "DELETE";
-  body?: any;
-  headers?: Record<string, string>;
-};
+// type FetchOptions = {
+//   method?: "GET" | "POST" | "PUT" | "DELETE";
+//   body?: any;
+//   headers?: Record<string, string>;
+// };
 
-export type registerFormData = Omit<IUser, "_id">;
-class ApiClient {
+// export type registerFormData = Omit<IUser, "_id">;
+// class ApiClient {
 
-    private async fetch<T>(endpoint: string,options: FetchOptions = {}): Promise<T> {
-        const { method = "GET", body, headers = {} } = options;
+//     private async fetch<T>(endpoint: string,options: FetchOptions = {}): Promise<T> {
+//         const { method = "GET", body, headers = {} } = options;
 
-        const defaultHeaders = {
-            "Content-Type": "application/json",
-            ...headers,
-        };
+//         const defaultHeaders = {
+//             "Content-Type": "application/json",
+//             ...headers,
+//         };
 
-        const response = await fetch(`/api${endpoint}`, {
-            method,
-            headers: defaultHeaders,
-            body: body ? JSON.stringify(body) : undefined,
-        });
+//         const response = await fetch(`/api${endpoint}`, {
+//             method,
+//             headers: defaultHeaders,
+//             body: body ? JSON.stringify(body) : undefined,
+//         });
 
-        if (!response.ok) {
-            throw new Error(await response.text());
-        }
+//         if (!response.ok) {
+//             throw new Error(await response.text());
+//         }
 
-        return response.json();
-    }
+//         return response.json();
+//     }
 
-    async registerUser(formData: registerFormData) {
-        return this.fetch<registerFormData>("/auth/register", {
-            method: "POST",
-            body: formData,
-        });
-    }
-}
+//     async registerUser(formData: registerFormData) {
+//         return this.fetch<registerFormData>("/auth/register", {
+//             method: "POST",
+//             body: formData,
+//         });
+//     }
+// }
 
-export const apiclient = new ApiClient();
+// export const apiclient = new ApiClient();
