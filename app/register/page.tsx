0090a7b57eaf9b147226c0bd1ee65c341f70d8f5
Sowparna Shome <soupornoshome@gmail.com>
2025-02-25 02:24:19 +0530

    bg remove

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -46,7 +46,7 @@ export default function Register() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-400 p-4">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-sm">
         <h1 className="text-2xl font-semibold text-center text-gray-700 mb-4">Register</h1>
         
