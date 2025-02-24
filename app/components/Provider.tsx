728d4d111b3963b91fb05de8d560d24354607099
Sowparna Shome <soupornoshome@gmail.com>
2025-02-24 21:50:22 +0530

    elements add

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -2,12 +2,15 @@
 import { ImageKitProvider } from "imagekitio-next";
 import { SessionProvider } from "next-auth/react";
 import { NotificationProvider } from "./NotificationPopup";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 
 const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
 const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY!;
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY!);
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-    const imagekitAuthenticator = async ()=>{
+    const imagekitAuthenticator = async () => {
         try {
             const response = await fetch("/api/auth-imagekit");
             if (!response.ok) throw new Error("❌ Failed to autheniticate imagekit");
@@ -19,11 +22,16 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     }
 
     return (
-        <SessionProvider refetchInterval={5 * 60}>
-            <NotificationProvider>
-                <ImageKitProvider publicKey={publicKey} urlEndpoint={urlEndpoint} authenticator={imagekitAuthenticator}>
-                    {children}
-                </ImageKitProvider>
-            </NotificationProvider>
-        </SessionProvider>);
+        <Elements stripe={stripePromise}>
+            <SessionProvider refetchInterval={5 * 60}>
+            
+                <NotificationProvider>
+                    <ImageKitProvider publicKey={publicKey} urlEndpoint={urlEndpoint} authenticator={imagekitAuthenticator}>
+                        {children}
+                    </ImageKitProvider>
+                </NotificationProvider>
+            
+            </SessionProvider>
+        </Elements>
+    );
 }
